84abae4001b59c2d44cc64dedb6668b58f944aef
ondrej <ondrejh.ck@email.cz>
2014-06-19 13:57:01 +0200

    wattage calculation

--- last_entry.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python3
-
-from dbconn import return_last_entry, get_raw_data
-
-#run main if this is stand alone module
-if __name__ == "__main__":
-    #print(return_last_entry())
-    print(get_raw_data(return_last_entry()))
---
+++ test_dbconn.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python3
+
+from dbconn import return_last_entry, return_data, get_raw_data
+
+import datetime
+
+def print_raw_entry(entry):
+
+    print('{}, '.format(str(entry[0])),end='')
+    
+    for c in entry[1:-1]:
+        print('{}, '.format(str(c)),end='')
+
+    print('{}'.format(str(entry[-1])))
+
+
+def calculate_wattage(data):
+
+    last_kwh = None
+    last_dtm = None
+    
+    for e in data:
+        if last_kwh == None:
+            last_dtm = e[0]
+            last_kwh = e[1]
+        else:
+            ddtm = e[0]-last_dtm
+            dkwh = e[1]-last_kwh
+            last_dtm = e[0]
+            last_kwh = e[1]
+            w = dkwh/(ddtm.total_seconds()/3600)
+            e.append(w)
+        
+
+#run main if this is stand alone module
+if __name__ == "__main__":
+
+    #last entry
+    print('Last entry:')
+    e = get_raw_data(return_last_entry())
+    print_raw_entry(e)
+
+    #all data
+    data = get_raw_data(return_data())
+    print('Data:')
+    calculate_wattage(data)
+    for e in data:
+        print_raw_entry(e)
+        #print(e)
+
