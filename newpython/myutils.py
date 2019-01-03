6d481bda4bf025e97d1b652eef5ac3dd54496854
Ondrej Hejda <ondrejh.ck@email.cz>
2019-01-03 21:10:13 +0100

    button inputs

--- newpython/myutils.py
+++ newpython/myutils.py
@@ -4,6 +4,10 @@ from subprocess import Popen, PIPE
 import RPi.GPIO as GPIO
 
 LCD_LIGHT_OUTPUT = 18
+BUTTON_1_INPUT = 5
+BUTTON_2_INPUT = 6
+BUTTON_3_INPUT = 13
+
 
 def get_my_ip():
 
@@ -13,17 +17,8 @@ def get_my_ip():
 	return myip.decode('ascii').strip()
 
 
-def lcd_light(status=True, init=False):
-
-	if init:
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setwarnings(False)
-		GPIO.setup(LCD_LIGHT_OUTPUT, GPIO.OUT)
-
-	GPIO.output(LCD_LIGHT_OUTPUT, 1 if status else 0)
-
-
 def get_onoff(prompt):
+	
 	while True:
 		try:
 			return {"on":True, "off":False}[input(prompt).lower()]
@@ -31,9 +26,49 @@ def get_onoff(prompt):
 			print("Invalid input please enter ON or OFF")
 
 
+class my_gpios():
+
+	def __init__(self):
+		
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setwarnings(False)
+		GPIO.setup(LCD_LIGHT_OUTPUT, GPIO.OUT)
+		GPIO.setup([BUTTON_1_INPUT, BUTTON_2_INPUT, BUTTON_3_INPUT], GPIO.IN) 
+
+	def lcd_light(self, status=True):
+
+		GPIO.output(LCD_LIGHT_OUTPUT, 1 if status else 0)
+
+	def get_buttons(self, btn=None):
+
+		if btn == 1:
+			return False if GPIO.input(BUTTON_1_INPUT) else True
+		if btn == 2:
+			return False if GPIO.input(BUTTON_2_INPUT) else True
+		if btn == 3:
+			return False if GPIO.input(BUTTON_3_INPUT) else True
+
+		btn1 = False if GPIO.input(BUTTON_1_INPUT) else True
+		btn2 = False if GPIO.input(BUTTON_2_INPUT) else True
+		btn3 = False if GPIO.input(BUTTON_3_INPUT) else True
+
+		return (btn1, btn2, btn3)
+
+
 if __name__ == "__main__":
+
+	gpios = my_gpios()
 
 	myip = get_my_ip()
 	print(myip)
 
-	lcd_light(get_onoff("LCD backlight status: "), True)
+	gpios.lcd_light(get_onoff("LCD backlight status: "))
+
+
+	from time import sleep
+
+	for i in range(10):
+
+		sleep(1)
+		btns = gpios.get_buttons()
+		print(btns)
