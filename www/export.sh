73132cdcc701d3421ac6d07e5d823bf854c0e15b
ondrejh <ondrejh.ck@email.cz>
2019-02-10 20:31:58 +0100

    use rsync instead of scp

--- www/export.sh
+++ www/export.sh
@@ -1,2 +1,2 @@
-scp * pi@192.168.1.220:/home/pi/www/
-ssh -t pi@192.168.1.220 "sudo cp /home/pi/www/* /var/www/html/"
+rsync -av --exclude='*.sh' --exclude='*.txt' ./ pi@192.168.1.220:/home/pi/www/
+ssh -t pi@192.168.1.220 "sudo rsync -av /home/pi/www/ /var/www/html/"
